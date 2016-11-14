61bfe3b1d6e355ec7d95382901af93328bbe60e9
KB429861 <kb429861@gmail.com>
2016-11-14 20:32:21 +0700

    Forms

--- UI/AreaForm.Designer.cs
+++ UI/AreaForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace UI
 {
-    partial class Form1
+    partial class AreaForm
     {
         /// <summary>
         /// Required designer variable.
@@ -30,10 +30,9 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.Text = "AreaForm";
         }
 
         #endregion
     }
 }
-
---
+++ UI/MainForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void NewAreaToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            new AreaForm().ShowDialog(this);
+        }
+    }
+}
