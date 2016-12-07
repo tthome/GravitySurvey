89c3ee3869081545b7f614f633b44e88d60f3e90
unknown <a.e.nesterenko.2011@pmi-310-04.corp.nstu.ru>
2016-12-07 17:14:37 +0700

    Modern UI

--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using System.Windows;
 using OxyPlot;
 using OxyPlot.Series;
-
+using Microsoft.Win32;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Media;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Linq;
 
 namespace UI
 {
@@ -20,7 +26,10 @@ namespace UI
 
         private void MenuItem_OnClick(object sender, RoutedEventArgs e)
         {
-            new AreaWindow().Show();
+            var openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog(this) == true)
+                ShowArea(openFileDialog.FileName);
+            //new AreaWindow().Show();
         }
 
         private void InputAreaButton_Click(object sender, RoutedEventArgs e)
@@ -38,11 +47,57 @@ namespace UI
             throw new System.NotImplementedException();
         }
 
-
         private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
         {
             PlotView.DataContext = null;
             PlotView.DataContext = this;
+        }
+
+        private void ShowArea(string path)
+        {
+            var values = LoadArea(path);
+            var min = values.SelectMany(row => row).Min();
+            var max = values.SelectMany(row => row).Max();
+            var isColumnsInitialized = false;
+            for (var row = 0; row < values.Count; row++)
+            {
+                AreaGrid.RowDefinitions.Add(new RowDefinition());
+                for (var column = 0; column < values[row].Length; column++)
+                {
+                    if (!isColumnsInitialized) AreaGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                    var percent = (max - values[row][column]) / (max - min);
+                    var textBlock = new TextBlock
+                    {
+                        Text = values[row][column].ToString("E2"),
+                        Background = new SolidColorBrush(
+                            Color.FromArgb((byte)(255 * percent), 39, 174, 96))
+                    };
+                    Grid.SetRow(textBlock, row);
+                    Grid.SetColumn(textBlock, column);
+                    AreaGrid.Children.Add(textBlock);
+                }
+                isColumnsInitialized = true;
+            }
+        }
+
+        private List<double[]> LoadArea(string path)
+        {
+            var list = new List<double[]>();
+            using (var reader = new StreamReader(path))
+            {
+                var regex = new Regex(@"[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?");
+                var line = reader.ReadLine();
+                while (!string.IsNullOrWhiteSpace(line))
+                {
+                    var matches = regex.Matches(line);
+                    var values = new double[matches.Count];
+                    for (var i = 0; i < matches.Count; i++)
+                        values[i] = Convert.ToDouble(matches[i].Value, CultureInfo.InvariantCulture);
+                    list.Add(values);
+                    line = reader.ReadLine();
+                }
+            }
+            return list;
         }
     }
 }
