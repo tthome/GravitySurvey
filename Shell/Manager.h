c4754559964d003a92f8059df12273b21cf15729
unknown <Kazanin.2015@pmi-310-03.corp.nstu.ru>
2016-12-07 16:59:22 +0700

    CLI :(

--- Shell/Manager.h
+++ Shell/Manager.h
@@ -1,14 +1,23 @@
 #ifndef MANAGER_H
 #define MANAGER_H
 
+#include <string>
+
 using namespace System;
 
-namespace Wrapper
+namespace Shell
 {
 	public ref class Manager
 	{
+		
 	public:
-		static void Run();
+		Manager();
+		~Manager();
+		void Run();
+		void InputArea(String^ path);
+
+	private:
+		Inverse inverse;
 	};
 }
 
